e726bb3927229c6a616732672d6b4b5c7c9436e8
Justin Miller <jayman1294@gmail.com>
2019-10-13 04:45:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,19 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+new_array = []
+array.each do |name|
+  new_array << name
+  
+end
+
+
+end
+
+def assign_rooms
+
+
+
+end
